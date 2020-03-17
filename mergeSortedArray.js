7b0d675422a7e0b90d552fe7fc8d8a86a48e4a24
luohaoliu <35145553+luohaoliu@users.noreply.github.com>
2020-03-17 19:21:37 -0400

    Update mergeSortedArray.js

--- mergeSortedArray.js
+++ mergeSortedArray.js
@@ -1,3 +1,7 @@
+
+// two sorted integer arrays nums1 and nums2
+// merge nums2 into nums1 as one sorted array
+
 var merge = function(nums1, m, nums2, n) {
   nums1.splice(m, n);
   nums1.push(...nums2);
