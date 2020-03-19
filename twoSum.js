bd684ce2ea7e3dbcac32cb16cfaa431d2b82b9d5
luohaoliu <35145553+luohaoliu@users.noreply.github.com>
2020-03-18 20:50:06 -0400

    Update twoSum.js

--- twoSum.js
+++ twoSum.js
@@ -1,4 +1,16 @@
 
+// an array of integers, return indices of the two numbers such that they add up to a specific target
+
+// assume that each input would have exactly one solution, may not use the same element twice
+
+// example
+
+// Given nums = [2, 7, 11, 15], target = 9,
+
+// Because nums[0] + nums[1] = 2 + 7 = 9,
+
+// return [0, 1].
+
 const twoSum = (nums, target) => {
 
   const map = {};
