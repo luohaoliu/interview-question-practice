b1453358648101196d4ebe2e2a48be0bbb039b09
luohaoliu <35145553+luohaoliu@users.noreply.github.com>
2020-03-18 20:50:30 -0400

    Update twoSum.js

--- twoSum.js
+++ twoSum.js
@@ -5,9 +5,9 @@
 
 // example
 
-// Given nums = [2, 7, 11, 15], target = 9,
+// given nums = [2, 7, 11, 15], target = 9,
 
-// Because nums[0] + nums[1] = 2 + 7 = 9,
+// because nums[0] + nums[1] = 2 + 7 = 9,
 
 // return [0, 1].
 
