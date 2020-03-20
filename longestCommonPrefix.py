52b48c835ee77ccbed983d8911c05aa19630a2d8
luohaoliu <35145553+luohaoliu@users.noreply.github.com>
2020-03-19 22:49:15 -0400

    Update longestCommonPrefix.py

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -1,2 +1,10 @@
 
 # write a function to find the longest common prefix string amongst an array of strings
+# if there is no common prefix, return an empty string ""
+
+# Input: ["flower","flow","flight"]
+# Output: "fl"
+
+# Input: ["dog","racecar","car"]
+# Output: ""
+# Explanation: There is no common prefix among the input strings
