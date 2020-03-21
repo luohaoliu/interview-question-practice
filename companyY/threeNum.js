a4fe89fb142ab8da6e81b6c264498b79b5b69b0b
luohaoliu <35145553+luohaoliu@users.noreply.github.com>
2020-03-21 09:06:33 -0400

    Update threeNum.js

--- companyY/threeNum.js
+++ companyY/threeNum.js
@@ -2,3 +2,28 @@
 // given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? 
 // find all unique triplets in the array which gives the sum of zero
 
+var threeNum = function(nums) {
+  nums.sort((a, b) => a - b);
+  const r = [];
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (nums[i] > 0) break;
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    let left = i + 1;
+    let right = nums.length - 1;
+    while (left < right) {
+      const s = nums[i] + nums[left] + nums[right];
+      if (s < 0) left += 1;
+      else if (s > 0) right -= 1;
+      else {
+        r.push([nums[i], nums[left], nums[right]]);
+        while (left < right && nums[left] === nums[left + 1]) left += 1;
+        while (left < right && nums[right] === nums[right - 1]) right -= 1;
+        left += 1;
+        right -= 1;
+      }
+    }
+  }
+  
+  return r;
+};
+
